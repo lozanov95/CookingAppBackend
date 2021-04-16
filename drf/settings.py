794513f685a4fda05cfb7b1f9ddc7a1dbe2bd8ca
lozanov95 <vasillozanov1@gmail.com>
2021-04-16 16:40:39 +0300

    installed django-heroku

--- drf/settings.py
+++ drf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,5 @@ CORS_ORIGIN_WHITELIST = [
 CORS_ORIGIN_REGEX_WHITELIST = [
     'http://localhost:3000',
 ]
+
+django_heroku.settings(locals())
