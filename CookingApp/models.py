0ce518234e259498704198a1adbe0cabb1330ba0
lozanov95 <vasillozanov1@gmail.com>
2021-04-16 18:31:39 +0300

    allowed 4096 characters on preparation steps

--- CookingApp/models.py
+++ CookingApp/models.py
@@ -20,7 +20,7 @@ class Recipe(models.Model):
     )
     name = models.CharField(max_length=128)
     ingredients = models.TextField(max_length=1024)
-    preparation_steps = models.TextField(max_length=1024)
+    preparation_steps = models.TextField(max_length=4096)
     difficulty = models.CharField(max_length=6, choices=DIFFICULTIES)
     image_url = models.URLField()
     created_time = models.DateTimeField(auto_now=True)
