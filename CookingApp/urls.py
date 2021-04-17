8066f6265f2ec58d7effb340935dfc04f6fb69f7
lozanov95 <vasillozanov1@gmail.com>
2021-04-17 12:58:02 +0300

    added auth token endpoint to the URLs

--- CookingApp/urls.py
+++ CookingApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .api import RecipeCreateApi, RecipeGetApi, RecipeDeleteApi, RecipeEditApi, RecipeGetByIdApi
 
 urlpatterns = [
+
     path('', RecipeGetApi.as_view()),
     path('<int:pk>', RecipeGetByIdApi.as_view()),
     path('create', RecipeCreateApi.as_view()),
--- drf/urls.py
+++ drf/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
+
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/recipes/', include('CookingApp.urls'))
+    path('api/recipes/', include('CookingApp.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
