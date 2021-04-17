b84f3792f8036d4a28eb71624926292561e9973a
lozanov95 <vasillozanov1@gmail.com>
2021-04-17 20:00:31 +0300

    removed password2, first_name and last_name from the register serializer

--- AccountApp/serializers.py
+++ AccountApp/serializers.py
@@ -11,21 +11,10 @@ class RegisterSerializer(serializers.ModelSerializer):
     )
 
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-        return attrs
+        fields = ('username', 'password', 'email')
 
     def create(self, validated_data):
         user = User.objects.create(
