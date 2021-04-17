19924edb8e6bb64cd6cf1e3392f144be6c6c9cb1
lozanov95 <vasillozanov1@gmail.com>
2021-04-17 18:20:11 +0300

    created custom token view - now returns user_id as well

--- AccountApp/urls.py
+++ AccountApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import CustomAuthToken
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='login')
+    path('login/', CustomAuthToken.as_view())
 ]
