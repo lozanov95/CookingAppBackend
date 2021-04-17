e66a9c704525d48be26d46c4784c695cb1113dbf
lozanov95 <vasillozanov1@gmail.com>
2021-04-17 20:14:08 +0300

    added logout functionality

--- AccountApp/urls.py
+++ AccountApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CustomAuthToken, RegisterView
+from .views import CustomAuthToken, RegisterView, api_logout
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('login/', CustomAuthToken.as_view()),
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('logout/', api_logout),
 ]
--- AccountApp/views.py
+++ AccountApp/views.py
@@ -1,9 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+
 from .serializers import RegisterSerializer
 
 
@@ -24,3 +27,11 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def api_logout(request):
+    request.user.auth_token.delete()
+    return Response(status=status.HTTP_200_OK)
