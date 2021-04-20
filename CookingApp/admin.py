30cec48c97f5f28253f0a844dc056918d1d73adc
lozanov95 <vasillozanov1@gmail.com>
2021-04-20 15:53:24 +0300

    added comments to the admin panel

--- CookingApp/admin.py
+++ CookingApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from CookingApp.models import Recipe
+from CookingApp.models import Recipe, Comment
 
 # Register your models here.
 
 admin.site.register(Recipe)
+admin.site.register(Comment)
